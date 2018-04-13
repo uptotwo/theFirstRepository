f6aa8bd8592a57ee2275f8f7bbcc75df7d13061c
Euron <lonerlydia@foxmail.com>
2018-04-13 09:03:54 +0800

    pro init
    Signed-off-by: Euron <lonerlydia@foxmail.com>

---
+++ MvcAndMybatisMaven/src/main/java/demo/dao/UserDao.java
@@ -0,0 +1,15 @@
+package demo.dao;
+
+import java.util.Set;
+
+import demo.entity.User;
+
+public interface UserDao {
+	
+	public User getByUsername(String username);
+
+    public Set<String> getRoles(String username);
+
+    public Set<String> getPermissions(String username);
+    
+}
