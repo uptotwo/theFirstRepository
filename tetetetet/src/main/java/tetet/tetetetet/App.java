d68f3050630a51366521e5d1becd7a92d9b88975
Euron <lonerlydia@foxmail.com>
2018-04-13 10:59:53 +0800

    似的发射点

---
+++ tetetetet/src/main/java/tetet/tetetetet/App.java
@@ -0,0 +1,13 @@
+package tetet.tetetetet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
